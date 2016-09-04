ccf706655b3aa95f01edde7ae289ee5f154a18d7
metherton <martinetherton63@gmail.com>
2016-09-04 17:10:51 +0200

    first commit

---
+++ app/scripts/services.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('confusionApp')
+    .constant("baseURL","http://localhost:3000/")
+
+    .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+            this.getDishes = function(){
+                return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
+            };
+
+            this.getPromotions = function(){
+                return $resource(baseURL+"promotions/:id",null,  {'update':{method:'PUT' }});
+            };
+
+        }])
+
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+            var corpfac = {};
+    
+            corpfac.getLeaders = function(){
+                return $resource(baseURL+"leadership/:id",null,  {'update':{method:'PUT' }});
+            };
+
+            return corpfac;
+        }])
+        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+            return $resource(baseURL +"feedback/:id", null);
+        }])
+
+;
